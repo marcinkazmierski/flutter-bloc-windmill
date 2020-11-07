9a192f1393b8a433e644dcefdf8c73b0afbf39d9
Marcin Kazmierski <m.kazmierski@expans.io>
2020-11-07 10:24:36 +0100

    windmill page view

--- lib/screens/windmill_page.dart
+++ lib/screens/windmill_page.dart
@@ -12,12 +12,19 @@ class WindmillPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: WindmillForm(),
+      body: WindmillForm(
+        accountModel: this.accountModel,
+      ),
     );
   }
 }
 
 class WindmillForm extends StatefulWidget {
+  final AccountModel accountModel;
+
+  const WindmillForm({@required this.accountModel})
+      : assert(accountModel != null);
+
   @override
   State<WindmillForm> createState() => _WindmillFormState();
 }
@@ -40,7 +47,56 @@ class _WindmillFormState extends State<WindmillForm> {
         builder: (context, state) {
           return Scaffold(
             body: Center(
-              child: Text('Main WindmillPage'),
+              child: Container(
+                  alignment: Alignment.center,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        ListTile(
+                          leading: Icon(Icons.wb_sunny_outlined, size: 50),
+                          title: Text('Your Windmill'),
+                          subtitle:
+                              Text('Name: ' + this.widget.accountModel.name),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(30),
+                          child: Image.asset('assets/images/windmill.png'),
+                        ),
+                      ])),
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                print("floatingActionButton onPressed");
+              },
+              tooltip: 'Add new',
+              child: Icon(Icons.add),
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: 0,
+              // this will be set when a new tab is tapped
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Colors.blueAccent,
+              selectedItemColor: Colors.white,
+              unselectedItemColor: Colors.white.withOpacity(.60),
+              selectedFontSize: 14,
+              unselectedFontSize: 14,
+              onTap: (value) {
+                print("BottomNavigationBarItem tap: " + value.toString());
+              },
+              items: [
+                BottomNavigationBarItem(
+                  icon: new Icon(Icons.home),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: new Icon(Icons.mail),
+                  label: 'Messages',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  label: 'Profile',
+                )
+              ],
             ),
           );
         },
