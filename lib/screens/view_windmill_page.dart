4d1158976352d5a23231676b0d77b9ffb47773f8
Marcin Kazmierski <m.kazmierski@expans.io>
2020-11-24 11:51:13 +0100

    LineReportChart - part.

--- lib/screens/view_windmill_page.dart
+++ lib/screens/view_windmill_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:windmill/blocs/windmill_bloc.dart';
 import 'package:windmill/models/account_model.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class ViewWindmillPage extends StatelessWidget {
   final AccountModel accountModel;
@@ -36,22 +37,49 @@ class _ViewWindmillFormState extends State<ViewWindmillForm> {
       builder: (context, state) {
         return Scaffold(
           body: Center(
-            child: Container(
-                alignment: Alignment.center,
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      ListTile(
-                        leading: Icon(Icons.wb_sunny_outlined, size: 50),
-                        title: Text('Your Windmill'),
-                        subtitle: Text('Name: ' +
-                            (state as WindmillLoadSuccess).windmillModel.name),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Text('Your Windmill',
+                      style: TextStyle(color: Colors.black, fontSize: 32)),
+                ),
+                SizedBox(
+                  height: 200.0,
+                  child: ListView.builder(
+                    physics: ClampingScrollPhysics(),
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 5,
+                    itemBuilder: (BuildContext context, int index) => Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text('Your Windmill'),
+                            Expanded(
+                              child: Image.asset('assets/images/windmill.png'),
+                            ),
+                          ],
+                        ),
                       ),
-                      Padding(
-                        padding: EdgeInsets.all(30),
-                        child: Image.asset('assets/images/windmill.png'),
-                      ),
-                    ])),
+                    ),
+                  ),
+                ),
+                Card(
+                  child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Text('Your Windmill'),
+                          LineReportChart(),
+                        ],
+                      )),
+                ),
+              ],
+            ),
           ),
           floatingActionButton: FloatingActionButton(
             onPressed: () {
@@ -65,6 +93,122 @@ class _ViewWindmillFormState extends State<ViewWindmillForm> {
           ),
         );
       },
+    );
+  }
+}
+
+class LineReportChart extends StatelessWidget {
+  List<Color> gradientColors = [
+    const Color(0xff23b6e6),
+    const Color(0xff02d39a),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2.2,
+      child: LineChart(mainData()),
+    );
+  }
+
+  List<FlSpot> getSports() {
+    return [
+      FlSpot(0, .5),
+      FlSpot(1, 1.5),
+      FlSpot(2, .5),
+      FlSpot(3, .7),
+      FlSpot(4, .2),
+      FlSpot(5, 2),
+      FlSpot(6, 1.5),
+      FlSpot(7, 1.7),
+      FlSpot(8, 1),
+      FlSpot(9, 2.8),
+      FlSpot(10, 2.5),
+      FlSpot(11, 2.65),
+    ];
+  }
+
+  LineChartData mainData() {
+    return LineChartData(
+      gridData: FlGridData(show: false),
+      titlesData: FlTitlesData(
+        show: true,
+        bottomTitles: SideTitles(
+          showTitles: true,
+          reservedSize: 22,
+          getTextStyles: (value) => const TextStyle(
+              color: Color(0xff68737d),
+              fontWeight: FontWeight.bold,
+              fontSize: 12),
+          getTitles: (value) {
+            switch (value.toInt()) {
+              case 1:
+                return 'Sty';
+              case 2:
+                return 'Lut';
+              case 3:
+                return 'Mar';
+              case 4:
+                return 'Kwi';
+              case 5:
+                return 'Maj';
+            }
+            return '';
+          },
+          margin: 8,
+        ),
+        leftTitles: SideTitles(
+          showTitles: true,
+          getTextStyles: (value) => const TextStyle(
+            color: Color(0xff67727d),
+            fontWeight: FontWeight.bold,
+            fontSize: 12,
+          ),
+          getTitles: (value) {
+            switch (value.toInt()) {
+              case 1:
+                return '10 tys';
+              case 3:
+                return '30 tys';
+              case 5:
+                return '50 tys';
+            }
+            return '';
+          },
+          reservedSize: 28,
+          margin: 12,
+        ),
+      ),
+      borderData: FlBorderData(show: false),
+      minX: 0,
+      maxX: 6,
+      minY: 0,
+      maxY: 6,
+      lineBarsData: [
+        LineChartBarData(
+          spots: [
+            FlSpot(0, 3),
+            FlSpot(1, 2),
+            FlSpot(2, 5),
+            FlSpot(3, 3.1),
+            FlSpot(4, 4),
+            FlSpot(5, 3),
+            FlSpot(6, 4),
+          ],
+          isCurved: true,
+          colors: gradientColors,
+          barWidth: 5,
+          isStrokeCapRound: true,
+          dotData: FlDotData(
+            show: false,
+          ),
+          belowBarData: BarAreaData(
+            show: true,
+            colors:
+                gradientColors.map((color) => color.withOpacity(0.3)).toList(),
+          ),
+        ),
+      ],
     );
   }
 }
